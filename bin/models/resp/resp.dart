1e817d6056ff3070c957ac8ced73a9a94911c3a6
m1 <saintsaintro@gmail.com>
2023-09-23 13:05:53 +0500

    get product list

---
+++ bin/models/resp/resp.dart
@@ -0,0 +1,13 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'resp.freezed.dart';
+part 'resp.g.dart';
+
+@freezed
+class resp with _$resp {
+  factory resp({
+    @Default([]) List<Product> products,
+  }) = _resp;
+
+  factory resp.fromJson(Map<String, dynamic> json) => _$respFromJson(json);
+}
--- bin/myapp.dart
+++ bin/myapp.dart
@@ -1,5 +1,13 @@
-import 'package:myapp/myapp.dart' as myapp;
+import 'package:dio/dio.dart';
 
-void main(List<String> arguments) {
-  print('Hello world: ${myapp.calculate()}!');
+import 'models/product/product.dart';
+import 'models/resp/resp.dart';
+
+void main(List<String> arguments) async {
+  var clientHttp = Dio();
+  var response = await clientHttp.get('https://dummyjson.com/products');
+
+  var respData = resp.fromJson(response.data);
+
+  print(respData.products);
 }
